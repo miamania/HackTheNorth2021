9ed7cb0188ea2e1413d8e8688c854e00a0b56915
Mia Berthier <miamania@gmail.com>
2021-01-15 19:25:17 -0500

    Initial Commit

---
+++ RubberDuck/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RubberDuck
+//
+//  Created by Mia Berthier on 2021-01-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
